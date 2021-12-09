20a7e2b17aaf7db06e8a24428c40cbef10ab09e7
hyacinth <hyacinthweng@gmail.com>
2021-12-09 02:33:35 -0500

    deleted

--- nav.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Studio Degen</a></li>
-        <li><a href="qsweeper.php">Q-Sweeper</a></li>
-        <li><a href="devlog.php">Devlog</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="shop.php">Shop</a></li>
-    </ul>
-</nav>
