ad46840bfef02c41d4784eced7f03eae81f742a1
Yiping-Dong <dongy551@newschool.edu>
2021-12-08 19:11:23 -0500

    nav bar

--- wireframe/scripts/main.js
+++ wireframe/scripts/main.js
@@ -3,3 +3,21 @@ document.querySelector('body').appendChild(newdiv);
 console.log('HI IM WORKING');
 
 const obj = { name: 'tm' };
+
+const navbar = document.getElementById('navbar');
+const logo = document.getElementById('logo');
+
+logo.addEventListener('mouseover',openNav);
+logo.addEventListener('mouseout',closeNav);
+navbar.addEventListener('mouseover',openNav);
+navbar.addEventListener('mouseout',closeNav);
+
+function openNav(){
+    navbar.style.height="212px";
+    logo.style.height="300px";
+}
+
+function closeNav(){
+    navbar.style.height="0px";
+    logo.style.height="100px";
+}
