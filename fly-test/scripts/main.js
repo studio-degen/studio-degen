0506a955e4f7fd50f542a8bb7f47e8dc135d5063
Apurv Rayate <rayateapurv@gmail.com>
2021-12-14 21:44:41 -0500

    flocking and basic frog

--- fly-test/scripts/main.js
+++ fly-test/scripts/main.js
@@ -1,5 +1,6 @@
 let flies = [];
 let flyimg1, flyimg2;
+let frog;
 
 function preload() {
     flyimg1 = loadImage('../assets/img/fly1.png');
@@ -8,7 +9,12 @@ function preload() {
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    for (let i = 0; i < 8; i++) {
+
+    noStroke();
+
+    frog = new Frog();
+
+    for (let i = 0; i < 20; i++) {
         flies.push(new Fly());       
     }
     console.log(flies);
@@ -16,75 +22,135 @@ function setup() {
 
 function draw() {
     clear();
-    let target = new p5.Vector(mouseX, mouseY);
+    //background(125);
+    noStroke();
+    fill('red');
+    rect(0, height - 400, 400, 400);
+
+    frog.show();
+
+    let mouse = new p5.Vector(mouseX, mouseY);
     flies.forEach((f) => {
         f.step();
         f.show();      
-        f.seek(target); 
-        f.separate(flies);
+        f.seekmouse(mouse); 
+        f.separation(flies);
+        f.alignment(flies);
+        f.cohesion(flies);
+
+        if(f.pos.x < 350 && f.pos.y > height - 350) {
+            frog.lash(f);
+            f.isDead = true;
+        }
     });
 
 }
 
+class Frog {
+    constructor() {
+        this.pos = createVector(70, height-70);
+        this.tongue = createVector(70, height-70);
+    }
+
+    show() {
+        fill(255);
+        circle(this.pos.x, this.pos.y, 20);
+        circle(this.tongue.x, this.tongue.y, 5);
+    }
+
+    lash(fly) {
+        // let dx = (fly.pos.x - this.tongue.x) * 0.01;
+        // let dy = (fly.pos.y - this.tongue.y) * 0.01;
+
+        // this.tongue.x += dx;
+        // this.tongue.y += dy;
+
+        // if (this.tongue.x == fly.pos.x && this.tongue.y == fly.pos.y) {
+        //     this.tongue = createVector(70, height-70);
+        // } 
+
+        stroke(0);
+        strokeWeight(2);
+        line(this.pos.x, this.pos.y, fly.pos.x, fly.pos.y);
+    }
+    
+}
+
+
 
 class Fly {
     constructor() {
-        this.pos = new p5.Vector(random(width), random(height));
-        this.vel = new p5.Vector(random(-1, 1), random(-1, 1));
-        this.field = 200;
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.acc = createVector(0, 0);
+        this.mousefield = 200;
+        this.flyfield = 70;
         this.maxspeed = 0.8;
+        this.maxforce = 0.05;
         this.sepdist = 30;
         this.perc = random(1);
+        this.isDead = false;
     }
     
     applyForce(force) {
-        this.vel.add(force);
+        this.acc.add(force);
     }
 
     step() {
-        this.vel.limit(this.maxspeed);
-        this.pos.add(this.vel);       
+        if(!this.isDead) {
+            this.vel.add(this.acc);
+            this.vel.limit(this.maxspeed);
+            this.pos.add(this.vel);
+            this.acc.mult(0);     
 
-        if (this.pos.x > width + 100) {
-            this.pos.x = -100;
-        } else if (this.pos.x < -100) {
-            this.pos.x = width + 100;
-        }
-        if (this.pos.y > height + 100) {
-            this.pos.y = -100;
-        } else if (this.pos.y < -100) {
-            this.pos.y = height + 100;
+            if (this.pos.x > width + 100) {
+                this.pos.x = -100;
+            } else if (this.pos.x < -100) {
+                this.pos.x = width + 100;
+            }
+            if (this.pos.y > height + 100) {
+                this.pos.y = -100;
+            } else if (this.pos.y < -100) {
+                this.pos.y = height + 100;
+            }
         }
     }
 
     show() {
-        // fill(0);
-        // circle(this.pos.x, this.pos.y, 10);
-        if(this.perc < 0.5) {
-            image(flyimg1, this.pos.x, this.pos.y, 20, 20);
-        } else {
-            image(flyimg2, this.pos.x, this.pos.y, 20, 20);
+        if(!this.isDead) {
+            // fill(0);
+            // circle(this.pos.x, this.pos.y, 10);
+            if(this.perc < 0.5) {
+                image(flyimg1, this.pos.x, this.pos.y, 20, 20);
+            } else {
+                image(flyimg2, this.pos.x, this.pos.y, 20, 20);
+            }
         }
     }
 
-    seek(target) {
+    seekmouse(mouse) {
         let steer = createVector(0, 0);
-        let d = p5.Vector.dist(this.pos, target);
-        if (d < this.field) {
-            let diff = p5.Vector.sub(target, this.pos);
+        let d = p5.Vector.dist(this.pos, mouse);
+        if (d < this.mousefield) {
+            let diff = p5.Vector.sub(mouse, this.pos);
             diff.normalize();
-            steer.add(diff);
-        }
-
-        if (steer.mag() > 0) {
-            steer.normalize();
-            steer.sub(this.vel);
-            steer.limit(this.maxspeed);
+            diff.mult(this.maxspeed);
+            steer = p5.Vector.sub(diff,this.vel);
+            steer.limit(this.maxforce); 
         }
         this.applyForce(steer);
     }
 
-    separate(boids) {
+    seek(target) {
+        let desired = p5.Vector.sub(target,this.pos);
+        desired.normalize();
+        desired.mult(this.maxspeed);
+        let steer = p5.Vector.sub(desired,this.vel);
+        steer.limit(this.maxforce); 
+        return steer;
+    }
+
+    separation(boids) {
         let steer = createVector(0, 0);
         let count = 0;
         // For every boid in the system, check if it's too close
@@ -105,8 +171,49 @@ class Fly {
 
         if (steer.mag() > 0) {
             steer.normalize();
-            steer.sub(this.vel);
-            steer.limit(this.maxspeed);
+            steer.mult(this.maxspeed);
+            steer.sub(this.velocity);
+            steer.limit(this.maxforce);
+        }
+        steer.mult(1.5);
+        this.applyForce(steer);
+    }
+
+    alignment(boids) {
+        let sum = createVector(0,0);
+        let count = 0;
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.pos ,boids[i].pos);
+            if ((d > 0) && (d < this.flyfield)) {
+            sum.add(boids[i].vel);
+            count++;
+            }
+        }
+        let steer = createVector(0, 0);
+        if (count > 0) {
+            sum.div(count);
+            sum.normalize();
+            sum.mult(this.maxspeed);
+            steer = p5.Vector.sub(sum, this.vel);
+            steer.limit(this.maxforce);
+        }
+        this.applyForce(steer);
+    }
+
+    cohesion(boids) {
+        let sum = createVector(0, 0);  
+        let count = 0;
+        for (let i = 0; i < boids.length; i++) {
+            let d = p5.Vector.dist(this.pos, boids[i].pos);
+            if ((d > 0) && (d < this.flyfield)) {
+            sum.add(boids[i].pos); 
+            count++;
+            }
+        }
+        let steer = createVector(0, 0);
+        if (count > 0) {
+            sum.div(count);
+            steer = this.seek(sum);
         }
         this.applyForce(steer);
     }
