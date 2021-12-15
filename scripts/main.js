f308ad691e0a600a041133447b3525e846d4a7f3
TanviMishra <misht490@newschool.edu>
2021-12-15 14:56:00 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -24,9 +24,9 @@ let flyimg1, flyimg2;
 let frog;
 
 function preload() {
-    flyimg1 = loadImage('../assets/expcam1');
-    flyimg2 = loadImage('../assets/flies/fly2.png');
-    frog = loadImage('../assets/flies/frog.png');
+    flyimg1 = loadImage('../assets/expcam1.png');
+    flyimg2 = loadImage('../assets/flies/expcam1.png');
+    frog = loadImage('../assets/flies/expcam1.png');
 }
 
 function setup() {
